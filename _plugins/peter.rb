9d26d0b0bb241e3a5ae7c1ab5666a64fe750189a
Peter Wang <happypeter1983@gmail.com>
2012-04-21 17:00:33 +0800

    i

---
+++ _plugins/peter.rb
@@ -0,0 +1,19 @@
+module Jekyll
+  class UpcaseConverter < Converter
+    safe true
+
+    priority :low
+
+    def matches(ext)
+      ext =~ /md/i
+    end 
+
+    def output_ext(ext)
+      ".html"
+    end
+
+    def convert(content)
+      content.upcase
+    end
+  end
+end
